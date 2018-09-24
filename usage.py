4c6348188aa722e077eb14af1be69f5429b5c892
Shammamah Hossain <shammamah.hossain@gmail.com>
2018-09-24 12:25:09 -0400

    Use Insulin for Dash app.

--- usage.py
+++ usage.py
@@ -9,48 +9,82 @@ app = dash.Dash('')
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
 
-sequence = 'MALWMRLLPLLALLALWGPDPAAAFVNQHLCGSHLVEALYLVCGERGFFYTPKTRREAEDLQVGQVELGGGPGAGSLQPLALEGSLQKRGIVEQCCTSICSLYQLENYCNMALWMRLLPLLALLALWGPDPAAAFVNQHLCGSHLVEALYLVCGERGFFYTPKTRREAEDLQVGQVELGGGPGAGSLQPLALEGSLQKRGIVEQCCTSICSLYQLENYCN'
+sequence = 'MALWMRLLPL LALLALWGPD PAAAFVNQHL CGSHLVEALY LVCGERGFFY TPKTRREAED LQVGQVELGG GPGAGSLQPL ALEGSLQKRG IVEQCCTSIC SLYQLENYCN'.replace(' ', '')
 
 highlightColor = 'blue'
 
-coverage = [
-    dict(
-        start=0,
-        end=11,
+cov_colors = {
+    "beta strand": {
+        "color": "#ff0000",
+        "bgcolor": "#0000ff",
+        "underscore": False,
+        "tooltip": "Beta strand"
+    },
+    "helix": {
+        "color": "#00ff00",
+        "bgcolor": "#000000",
+        "underscore": True,
+        "tooltip": "Alpha helix"
+    },
+    "turn": {
+        "color": "#0f0f0f",
+        "bgcolor": "#ffff00",
+        "underscore": True,
+        "tooltip": "Turn"
+    }
+}
+
+sec_structure = [
+    [0, 26],
+    [26, 29, "beta strand"],
+    [29, 33],
+    [33, 43, "helix"],
+    [43, 44],
+    [44, 46, "helix"],
+    [46, 48],
+    [48, 50, "beta strand"],
+    [50, 56],
+    [56, 58, "beta strand"],
+    [58, 59],
+    [59, 66, "turn"],
+    [66, 74],
+    [74, 76, "beta strand"],
+    [76, 79],
+    [79, 81, "helix"],
+    [81, 84],
+    [84, 86, "turn"],
+    [86, 91],
+    [91, 97, "helix"],
+    [97, 98],
+    [98, 101, "beta strand"],
+    [101, 102],
+    [102, 106, "helix"],
+    [106, 107],
+    [107, 109, "turn"]
+]
+
+coverage = []
+
+for s in sec_structure:
+    section_dict = dict(
+        start=s[0],
+        end=s[1],
         color="#000000",
-        bgcolor="#00ff00",
-        tooltip="hi",
-        underscore=False
-    ),
-    dict(
-        start=11,
-        end=23,
-        color="#ff0000",
-        bgcolor="#0000ff",
-        underscore=True
-    ),
-    dict(
-        start=23,
-        end=50,
-        color="#ff00ff",
         bgcolor="#ffffff",
         underscore=False
-    ),
-    dict(
-        start=50,
-        end=len(sequence) - 1,
-        color="#0f0f0f",
-        bgcolor="#ff00d1",
-        underscore=False
     )
-]
+    if(len(s) > 2):
+        settings = cov_colors[s[2]]
+        for key in settings:
+            section_dict[key] = settings[key]
+    coverage.append(section_dict)
 
 selection = [10, 20, 'blue']
 
 app.layout = html.Div([
     dash_bio.SequenceViewerComponent(
         id='sequence-viewer',
-        title="This is a protein",
+        title="Insulin ",
         wrapAminoAcids=True,
         search=True,
         sequence=sequence,
@@ -78,14 +112,6 @@ app.layout = html.Div([
     
  
 @app.callback(
-    Output('sequence-viewer', 'title'),
-    [Input('test-input', 'value')]
-)
-def update_selection_low_high(v):
-    return v
-
-
-@app.callback(
     Output('sequence-viewer', 'selection'),
     [Input('slider', 'value')]
 )
@@ -99,6 +125,12 @@ def update_sel(v):
 )
 def update_coverage(v):
     cov = coverage[v]
+    for s in sec_structure:
+        if(s[0] == cov['start']):
+            if(len(s) > 2):
+                return cov['tooltip']
+            else:
+                return ""
     return sequence[cov['start']:cov['end']]
 
 
