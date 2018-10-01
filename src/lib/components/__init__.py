dbbf28a41fe8e602698dfe3b7001c2f99a7ac4d8
Bachibouzouk <pfduc@physics.mcgill.ca>
2018-10-01 16:53:57 +0200

    added the files from their individual repositories

---
+++ src/lib/components/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# Require that numpy exists for figure_factory
+import numpy
+
+from _manhattan import create_manhattan
+from _volcano import create_volcano
