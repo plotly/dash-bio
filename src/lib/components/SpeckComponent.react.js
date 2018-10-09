309dd0ef4d4f405eccd6499ba3f5cad0d3f6ca15
Shammamah Hossain <shammamah.hossain@gmail.com>
2018-10-09 14:48:06 -0400

    Begin adding code to make Speck renderer work as a React component. Begin adding code to make Speck renderer work as a React component. Please enter the commit message for your changes. Lines starting

--- src/lib/components/SpeckComponent.react.js
+++ src/lib/components/SpeckComponent.react.js
@@ -3,22 +3,59 @@ import PropTypes from 'prop-types';
 
 
 const speckRenderer = require('../speck/src/renderer.js'); 
+const speckSystem = require('../speck/src/system.js');
+const speckView = require('../speck/src/view.js');
 
 export default class SpeckComponent extends Component {
+
+    loadStructure(data, renderer, view) {
+	var system = speckSystem.new();
+	for(var i = 0; i < data.length; i++) {
+	    // get the coordinate data
+	    var a = data[i];
+	    var x = a['x'];
+	    var y = a['y'];
+	    var z = a['z'];
+
+	    // add to the system
+	    speckSystem.addAtom(system, a.symbol, x, y, z);
+	}
+	speckSystem.center(system);
+	// bonds are calculated based on whether the distance between
+	// two adjacent atoms is smaller than some function of the
+	// maximum atomic radius
+	speckSystem.calculateBonds(system);
+	// the view refers to the parameters of, e.g., atom shade, etc.
+	renderer.setSystem(system, view);
+    }
+
     
     constructor(props) {
 	super('props');
+	var system = speckSystem.new();
+	this.loadStructure = this.loadStructure.bind(this);
     }
     
     componentDidMount() {
+	const {data,
+	       view} = this.props;
 	const canvas = this.refs.canvas;
-	var r = new speckRenderer(this.refs.canvas, 200, 200); 
+
+	var renderer = new speckRenderer(this.refs.canvas, 200, 200);
+
+	this.props.setProps({
+	    view: speckView.new()
+	})
+	renderer.initialize();
+	console.warn(view);
+	this.loadStructure(data, renderer, view);
     }
 
     render() {
 	const {
 	    id,
-	    label
+	    label,
+	    view
 	} = this.props;
 
 	return (
@@ -35,6 +72,38 @@ SpeckComponent.propTypes = {
 
     id: PropTypes.string,
     label: PropTypes.string,
-    setProps: PropTypes.func
+    data: PropTypes.arrayOf(PropTypes.shape({
+	x: PropTypes.number,
+	y: PropTypes.number,
+	z: PropTypes.number,
+	symbol: PropTypes.string
+    })),
+    view: PropTypes.shape({
+	aspect: PropTypes.number,
+	zoom: PropTypes.number,
+	translation: PropTypes.shape({
+	    x: PropTypes.number,
+	    y: PropTypes.number,
+	}),
+	atomScale: PropTypes.number,
+	relativeAtomScale: PropTypes.number,
+	bondScale: PropTypes.number,
+	rotation: PropTypes.func,
+	ao: PropTypes.number,
+	aoRes: PropTypes.number,
+	brightness: PropTypes.number,
+	outline: PropTypes.number,
+	spf: PropTypes.number,
+	bonds: PropTypes.bool,
+	bondThreshold: PropTypes.number,
+	bondShade: PropTypes.number,
+	atomShade: PropTypes.number,
+	resolution: PropTypes.number,
+	dofStrength: PropTypes.number,
+	dofPosition: PropTypes.number,
+	fxaa: PropTypes.number
+    }),
+    setProps: PropTypes.func,
+    
 
 }
