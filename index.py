22115c3c924a13a547e50055f2485a3db0fa03f8
Matthew Chan <matthew.chan@shaw.ca>
2018-10-10 12:29:13 -0400

    added callback to tests and css asset folder

--- index.py
+++ index.py
@@ -24,7 +24,24 @@ apps = {
     if filename.startswith("app_") and filename.endswith(".py")
 }
 
+for key in apps:
+    apps[key].callbacks(app)
 
+'''
+Ensure you add an html.Div holding an empty instance of your component to
+app.layout
+
+Ex:
+app.layout = html.Div(
+    id="index-waitfor",
+    children=[
+        dcc.Location(id="location"),
+        html.Div(id="container"),
+        html.Div(style={"display": "none"}, children=dcc.Graph(id="graph")),
+        html.Div(style={"display": "none"}, children=dash_bio.Component()),
+    ]
+)
+'''
 app.layout = html.Div(
     id="index-waitfor",
     children=[
