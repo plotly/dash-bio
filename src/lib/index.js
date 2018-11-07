5ad27359d73e80c456916b84c7d1183db2081b1e
Shammamah Hossain <shammamah.hossain@gmail.com>
2018-11-07 16:29:22 -0500

    Update index.js to exclude example component.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable import/prefer-default-export */
-import ExampleComponent from './components/ExampleComponent.react';
 import SpeckComponent from './components/SpeckComponent.react';
 
 export {
-    ExampleComponent,
     SpeckComponent
 };
