947965938c9b64197ccc2938030449ed9f9abd9c
matthewchan <matthew.chan@shaw.ca>
2018-11-22 20:35:57 -0700

    Update build

--- dash_bio/_imports_.py
+++ dash_bio/_imports_.py
@@ -1,6 +1,8 @@
+from .DashCircos import DashCircos
 from .NeedlePlot import NeedlePlot
 
 
 __all__ = [
+    "DashCircos",
     "NeedlePlot",
 ]
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import NeedlePlot from './components/NeedlePlot.react';
+import DashCircos from './components/DashCircos.react';
 export {
     NeedlePlot,
+    DashCircos
 };
