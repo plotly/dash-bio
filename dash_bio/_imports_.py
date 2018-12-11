d33f77b91cf6206b8dc716ccae933ce6ee158041
Shammamah Hossain <shammamah.hossain@gmail.com>
2018-12-11 13:24:17 -0500

    Register onClick events immediately when coverage is loaded.

--- dash_bio/_imports_.py
+++ dash_bio/_imports_.py
@@ -3,9 +3,10 @@ from .DashIdeogram import DashIdeogram
 from .NeedlePlot import NeedlePlot
 from .SequenceViewer import SequenceViewer
 
+
 __all__ = [
     "DashCircos",
     "DashIdeogram",
     "NeedlePlot",
-    "SequenceViewer"
+    "SequenceViewer",
 ]
