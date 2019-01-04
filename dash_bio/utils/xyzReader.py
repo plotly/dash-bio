5d23530686958cb6f81ecff67f2b740eb8476326
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-01-04 13:35:44 -0500

    Add docstrings and comments to XYZ file reader.

--- dash_bio/utils/xyzReader.py
+++ dash_bio/utils/xyzReader.py
@@ -1,10 +1,26 @@
 import tempfile
 import re
 
+
 def readXYZ(
         filePath='',
         dataString=''
 ):
+    '''
+    Reads .xyz files from either a raw string or a
+    relative file path. 
+
+    :param (string) filePath: The relative filepath to
+                              the .xyz file. 
+    :param (string) dataString: A raw string containing 
+                                the contents of the .xyz 
+                                file.
+
+    :rtype (list): A list of the atoms in the order that 
+                   they appear on the file, stored in 
+                   objects with keys "symbol", "x", "y",
+                   and "z". 
+    '''
     atoms = []
 
     if(len(filePath) > 0 and len(dataString) > 0):
@@ -24,11 +40,18 @@ def readXYZ(
         tf.write('\n'.join(lines))
     
     with open(tf.name, 'r') as f:
-        for line in lines: 
+        for line in lines:
+            # each line in an xyz file contains the symbol for the atom at
+            # that position, as well as the x, y, and z coordinates of the
+            # atom separated by spaces
+            # the coordinates can have signs (+/-), and decimals (.).
+            # an example line in a xyz file:
+            # C    +0.125    -1.032    +2.000
             r = re.search(
                 r'^([\w]+)\s+([\w\.\+\-]+)\s+([\w\.\+\-]+)\s+([\w\.\+\-]+)',
                 line)
-            
+
+            # pass if the line does not contain this information
             if r is None or len(r.groups()) < 4:
                 continue
             
