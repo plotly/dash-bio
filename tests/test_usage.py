b56f36dc1d2a0f2c6d69736a9bf2a74455f514ef
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-01-07 15:48:22 -0500

    Start with same integration test as dash-canvas

---
+++ tests/test_usage.py
@@ -0,0 +1,25 @@
+import shutil
+import sys
+
+
+from pytest_dash.utils import (
+    import_app,
+    wait_for_text_to_equal,
+    wait_for_element_by_css_selector
+)
+
+
+def test_install(cookies, dash_threaded, selenium):
+    results = cookies.bake(extra_context={
+        'project_name': 'DashCanvas',
+        'author_name': 'test',
+        'author_email': 'test',
+    })
+
+    # Add the generated project to the path so it can be loaded from usage.py
+    # It lies somewhere in a temp directory created by pytest-cookies
+    sys.path.insert(0, str(results.project))
+
+    # Test that `usage.py` works after building the default component.
+    dash_threaded(import_app('index'))
+
