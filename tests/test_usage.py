92a6eb21b5c9e5a8171d31ab82a5fb7048951f27
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-01-08 18:11:29 -0500

    Add docstring and update comments

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -10,16 +10,22 @@ from pytest_dash.utils import (
 
 
 def test_install(cookies, dash_threaded, selenium):
+    """Launch the app.
+
+    `dash_threaded` is a fixture by `pytest-dash`.
+    It will load a .py file containing a Dash instance
+    and start it in a thread.
+    """
     results = cookies.bake(extra_context={
         'project_name': 'Dash Bio',
         'author_name': 'test',
         'author_email': 'test',
     })
 
-    # Add the generated project to the path so it can be loaded from usage.py
+    # Add the generated project to the path
     # It lies somewhere in a temp directory created by pytest-cookies
     sys.path.insert(0, str(results.project))
 
-    # Test that `usage.py` works after building the default component.
+    # Test that `index.py` (the main gallery) works
     dash_threaded(import_app('index'))
 
