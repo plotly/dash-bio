17ba4bd6eadd3c1292e03263b55c78ad4b3d6cb5
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-01-09 15:47:47 -0500

    Rename test and drop cookies fixture

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -9,22 +9,13 @@ from pytest_dash.utils import (
 )
 
 
-def test_install(cookies, dash_threaded, selenium):
+def test_start(dash_threaded, selenium):
     """Launch the app.
 
     `dash_threaded` is a fixture by `pytest-dash`.
     It will load a .py file containing a Dash instance
     and start it in a thread.
     """
-    results = cookies.bake(extra_context={
-        'project_name': 'Dash Bio',
-        'author_name': 'test',
-        'author_email': 'test',
-    })
-
-    # Add the generated project to the path
-    # It lies somewhere in a temp directory created by pytest-cookies
-    sys.path.insert(0, str(results.project))
 
     # Test that `index.py` (the main gallery) works
     dash_threaded(import_app('index'))
