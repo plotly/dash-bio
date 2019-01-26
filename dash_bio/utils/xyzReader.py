ed103677358adaf6bbe05169f473ff31536087ac
pierre-francois.duc <pierre-francois.duc@netplus.ch>
2019-01-26 16:06:58 +0100

    Fix linting and flake8 in xyzReader.py

--- dash_bio/utils/xyzReader.py
+++ dash_bio/utils/xyzReader.py
@@ -2,43 +2,43 @@ import tempfile
 import re
 
 
-def readXYZ(
-        filePath='',
-        dataString=''
+def read_xyz(
+        file_path='',
+        data_string=''
 ):
-    '''
+    """
     Reads .xyz files from either a raw string or a
-    relative file path. 
+    relative file path.
 
-    :param (string) filePath: The relative filepath to
-                              the .xyz file. 
-    :param (string) dataString: A raw string containing 
-                                the contents of the .xyz 
+    :param (string) file_path: The relative filepath to
+                              the .xyz file.
+    :param (string) data_string: A raw string containing
+                                the contents of the .xyz
                                 file.
 
-    :rtype (list): A list of the atoms in the order that 
-                   they appear on the file, stored in 
+    :rtype (list): A list of the atoms in the order that
+                   they appear on the file, stored in
                    objects with keys "symbol", "x", "y",
-                   and "z". 
-    '''
+                   and "z".
+    """
     atoms = []
 
-    if(len(filePath) > 0 and len(dataString) > 0):
+    if len(file_path) > 0 and len(data_string) > 0:
         raise Exception(
             "Please specify either a file path or a \
             string of data."
         )
 
-    if(len(filePath) > 0):
-        with open(filePath, 'r') as f:
+    if len(file_path) > 0:
+        with open(file_path, 'r') as f:
             lines = f.readlines()
 
     else:
-        lines = dataString.split('\n')
+        lines = data_string.split('\n')
 
     with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tf:
         tf.write('\n'.join(lines))
-    
+
     with open(tf.name, 'r') as f:
         for line in lines:
             # each line in an xyz file contains the symbol for the atom at
@@ -54,14 +54,14 @@ def readXYZ(
             # pass if the line does not contain this information
             if r is None or len(r.groups()) < 4:
                 continue
-            
+
             atom = {
                 'symbol': r.group(1),
                 'x': float(r.group(2)),
                 'y': float(r.group(3)),
                 'z': float(r.group(4))
             }
-            
+
             atoms.append(atom)
 
     return atoms
