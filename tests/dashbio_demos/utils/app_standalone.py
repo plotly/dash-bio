14b502338fba74540cb9640be0ccffddb4e8e2af
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-01-29 19:24:55 +0100

    Write helper function to run app as standalone
    Co-authored-by: Pierre Francois <pierre-francois.duc@netplus.ch>
    Co-authored-by: Shammamah Hossain <shammamah.hossain@gmail.com>

---
+++ tests/dashbio_demos/utils/app_standalone.py
@@ -0,0 +1,12 @@
+import dash
+
+
+def run_standalone_app(layout, callbacks, port=8050, debug=True):
+    """Run demo app (tests/dashbio_demos/app_*.py) as standalone app."""
+    app = dash.Dash()
+    app.scripts.config.serve_locally = True
+    app.config['suppress_callback_exceptions'] = True
+
+    app.layout = layout()  # assign layout
+    callbacks(app)  # register all callbacks
+    app.run_server(debug=debug, port=port)
