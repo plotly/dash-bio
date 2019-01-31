f46e818d235429861cc99df262a8f3b5dda8a699
pierre-francois.duc <pierre-francois.duc@netplus.ch>
2019-01-31 18:09:38 +0100

    Comment the decorator definition for accessing the demo app pages in the tests

--- tests/test_common_features.py
+++ tests/test_common_features.py
@@ -6,7 +6,7 @@ from pytest_dash.utils import (
 
 
 def access_demo_app(dash_threaded, selenium, app_name):
-    """mimic a user click on the app link from the gallery"""
+    """Mimic a user click on the app link from the gallery."""
     dash_bio_index = import_app('index')
     dash_threaded(dash_bio_index)
     link = wait_for_element_by_id(selenium, 'app-link-id-{}'.format(app_name))
@@ -14,10 +14,14 @@ def access_demo_app(dash_threaded, selenium, app_name):
 
 
 def init_demo_app(app_name):
-    def access_demo_app_wrapper(func):
+    # allow the decorator to take the app_name as an argument
+    def decorator_init_demo_app(func):
+        # preserves the __name__ and __doc__ values of the decorated func
         @functools.wraps(func)
-        def wrapper(dash_threaded, selenium):
+        def wrapper_init_demo_app(dash_threaded, selenium):
+            # start the index.py app in a thread and move to the component URL
             access_demo_app(dash_threaded, selenium, app_name)
+            # execute the test function from there
             func(dash_threaded, selenium)
-        return wrapper
-    return access_demo_app_wrapper
+        return wrapper_init_demo_app
+    return decorator_init_demo_app
