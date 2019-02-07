14b3e4b9afd565864a5907327c458f813953d6b8
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-02-07 16:13:37 -0500

    Change dcc.Link to html.A.

--- tests/dashbio_demos/utils/app_wrapper.py
+++ tests/dashbio_demos/utils/app_wrapper.py
@@ -17,7 +17,7 @@ def app_page_layout(page_layout,
             html.Div(
                 id='app-page-header',
                 children=[
-                    dcc.Link(
+                    html.A(
                         html.Img(
                             src='data:image/png;base64,{}'.format(
                                 base64.b64encode(
