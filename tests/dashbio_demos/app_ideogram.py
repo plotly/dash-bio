9e47f5f32349916a146fc70062e0ad58b1671a0d
pierre-francois.duc <pierre-francois.duc@netplus.ch>
2019-02-26 23:53:18 +0100

    Fix Pylint error

--- tests/dashbio_demos/app_ideogram.py
+++ tests/dashbio_demos/app_ideogram.py
@@ -3,7 +3,6 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_bio
-from dash_bio.utils import ideogram_parser as ideoParser
 
 # running directly with Python
 if __name__ == '__main__':
