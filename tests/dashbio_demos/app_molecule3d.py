563ee307bafa47fca2df70b34537a13d43cc0ed7
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-03-01 16:52:18 -0500

    Group dash bio imports.

--- tests/dashbio_demos/app_molecule3d.py
+++ tests/dashbio_demos/app_molecule3d.py
@@ -6,9 +6,11 @@ from shutil import copy2
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
-import dash_bio
 import dash_daq as daq
+import dash_bio
 from dash_bio.utils import pdb_parser as parser, styles_parser as sparser
+
+
 
 # running directly with Python
 if __name__ == '__main__':
