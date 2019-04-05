e36904d9f1e23a76fca402374615c77c44f03c5e
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2019-04-05 18:32:53 +0200

    Fix pylint errors

--- tests/dashbio_demos/app_genome_viewer.py
+++ tests/dashbio_demos/app_genome_viewer.py
@@ -1,9 +1,6 @@
-from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
 import dash_html_components as html
 import dash_bio
 import flask
-import glob
 import os
 
 data_directory = 'tests/dashbio_demos/sample_data'
@@ -17,22 +14,23 @@ def header_colors():
 
 
 def description():
-    return "An interactive genome viewer with multiple tracks, aimed at investigating genomic variants."
+    return 'An interactive genome viewer with multiple tracks, aimed at investigating genomic ' \
+        'variants.'
 
 
 def layout():
     return html.Div(id='genome-body', children=[
-            dash_bio.GenomeViewer(
+        dash_bio.GenomeViewer(
             id='genome-viewer',
-	    genomedata="https://www.biodalliance.org/datasets/hg19.2bit",
+            genomedata='https://www.biodalliance.org/datasets/hg19.2bit',
             trackdata='/static/synth3.normal.17.7500000-7515000.bam',
             trackindex='/static/synth3.normal.17.7500000-7515000.bam.bai',
             variantdata='/static/snv.chr17.vcf',
             genedata="https://www.biodalliance.org/datasets/ensGene.bb",
             contig="chr17",
             start=7512284,
-            stop=7512644,
-                )
+            stop=7512644
+        )
 
     ])
 
@@ -42,6 +40,5 @@ def callbacks(app):
     @app.server.route('{}<data_path>'.format(static_image_route))
     def serve_image(data_path):
         if data_path not in list_of_data:
-            raise Exception(
-        '"{}" is excluded from the allowed static files'.format(data_path))
+            raise Exception('"{}" is excluded from the allowed static files'.format(data_path))
         return flask.send_from_directory(data_directory, data_path)
