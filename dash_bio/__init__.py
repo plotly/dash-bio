01d614f07984cd5010084d8386c427e9c39e6816
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-05-08 15:41:50 -0400

    Update unpkg URL.

--- dash_bio/__init__.py
+++ dash_bio/__init__.py
@@ -37,7 +37,7 @@ _js_dist = [
     {
         'relative_package_path': 'bundle.js',
         'external_url': (
-            'https://unpkg.com/dash_bio'
+            'https://unpkg.com/dash-bio'
             '/' + package_name + '/bundle.js'
         ).format(__version__),
         'namespace': package_name
