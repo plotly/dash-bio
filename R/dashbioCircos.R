28c375bea4306750a8ae72c740d28f3281c02c29
Ryan Patrick Kyle <rpkyle@users.noreply.github.com>
2019-05-24 15:04:54 -0400

    Initial build of R package to implement dash-bio functionality for DashR (#351)
    * :package: initial R package commit
    
    * :pencil: minor edit to OncoPrint.react.js docstring to fix transpilation fault
    
    * Update src/lib/components/OncoPrint.react.js
    
    Co-Authored-By: Marianne Corvellec <marianne.corvellec@ens-lyon.org>
    
    * :pencil: updated OncoPrint and docs to :hocho: %
    
    * :pencil: updated R docs to pull in docstrings
    
    * Update .Rbuildignore
    
    Rename test/ to tests/
    
    Co-Authored-By: Marianne Corvellec <marianne.corvellec@ens-lyon.org>
    
    * Ignore Python files during R package build
    
    * :books: update documentation

---
+++ R/dashbioCircos.R
@@ -0,0 +1,17 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+dashbioCircos <- function(enableDownloadSVG=NULL, enableZoomPan=NULL, id=NULL, style=NULL, eventDatum=NULL, selectEvent=NULL, layout=NULL, config=NULL, size=NULL, tracks=NULL) {
+    
+    component <- list(
+        props = list(enableDownloadSVG=enableDownloadSVG, enableZoomPan=enableZoomPan, id=id, style=style, eventDatum=eventDatum, selectEvent=selectEvent, layout=layout, config=config, size=size, tracks=tracks),
+        type = 'Circos',
+        namespace = 'dash_bio',
+        propNames = c('enableDownloadSVG', 'enableZoomPan', 'id', 'style', 'eventDatum', 'selectEvent', 'layout', 'config', 'size', 'tracks'),
+        package = 'dashBio'
+        )
+
+    component$props <- filter_null(component$props)
+
+    structure(component, class = c('dash_component', 'list'))
+}
+
---
+++ R/dashbioOncoPrint.R
@@ -0,0 +1,17 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+dashbioOncoPrint <- function(id=NULL, eventDatum=NULL, data=NULL, padding=NULL, colorscale=NULL, backgroundcolor=NULL, range=NULL, showlegend=NULL, showoverview=NULL, width=NULL, height=NULL) {
+    
+    component <- list(
+        props = list(id=id, eventDatum=eventDatum, data=data, padding=padding, colorscale=colorscale, backgroundcolor=backgroundcolor, range=range, showlegend=showlegend, showoverview=showoverview, width=width, height=height),
+        type = 'OncoPrint',
+        namespace = 'dash_bio',
+        propNames = c('id', 'eventDatum', 'data', 'padding', 'colorscale', 'backgroundcolor', 'range', 'showlegend', 'showoverview', 'width', 'height'),
+        package = 'dashBio'
+        )
+
+    component$props <- filter_null(component$props)
+
+    structure(component, class = c('dash_component', 'list'))
+}
+
