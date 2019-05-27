724959d99f35822af69564c53a273723308e2aec
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-05-27 16:46:14 -0400

    Fix pylint errors.

--- tests/dashbio_demos/app_molecule2d.py
+++ tests/dashbio_demos/app_molecule2d.py
@@ -1,18 +1,11 @@
 import os
-import base64
-import json
-import tempfile
 
-from shutil import copy2
-from textwrap import dedent as s
-
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as dcc
-import dash_daq as daq
 
-import dash_bio
 from dash_bio_utils.chem_structure_reader import read_structure
+import dash_bio
 
 # running directly with Python
 if __name__ == '__main__':
@@ -21,37 +14,6 @@ if __name__ == '__main__':
 # running with gunicorn (on servers)
 elif 'DASH_PATH_ROUTING' in os.environ:
     from tests.dashbio_demos.utils.app_standalone import run_standalone_app
-
-
-residue = {
-  'nodes': [
-    {"id":1000,"atom":"HE22"},
-    {"id":991,"atom":"C"},
-    {"id":997,"atom":"NE2"},
-    {"id":990,"atom":"CA"},
-    {"id":995,"atom":"CD"},
-    {"id":996,"atom":"OE1"},
-    {"id":993,"atom":"CB"},
-    {"id":992,"atom":"O"},
-    {"id":994,"atom":"CG"},
-    {"id":999,"atom":"HE21"},
-    {"id":989,"atom":"N"},
-    {"id":998,"atom":"H"}
-  ],
-  'links': [
-    {"id": 90, "source":989,"target":990,"bond":1, "strength": 1, "distance": 30.0 },
-    {"id": 91, "source":989,"target":998,"bond":1, "strength": 1, "distance": 30.0 },
-    {"id": 92, "source":990,"target":991,"bond":1, "strength": 1, "distance": 30.0 },
-    {"id": 93, "source":990,"target":993,"bond":1, "strength": 1, "distance": 30.0 },
-    {"id": 94, "source":991,"target":992,"bond":2, "strength": 1, "distance": 30.0 },
-    {"id": 95, "source":993,"target":994,"bond":1, "strength": 1, "distance": 30.0 },
-    {"id": 96, "source":994,"target":995,"bond":1, "strength": 1, "distance": 30.0 },
-    {"id": 97, "source":995,"target":997,"bond":1, "strength": 1, "distance": 30.0 },
-    {"id": 98, "source":995,"target":996,"bond":2, "strength": 1, "distance": 30.0 },
-    {"id": 99, "source":997,"target":1000,"bond":1, "strength": 1, "distance": 30.0 },
-    {"id": 100, "source":997,"target":999,"bond":1, "strength": 1, "distance": 30.0 }
-  ],
-}
 
 DATAPATH = os.path.join(".", "tests", "dashbio_demos", "sample_data", "mol2d_")
 
@@ -83,8 +45,7 @@ def layout():
     ])
 
 
-def callbacks(app):
-
+def callbacks(app):  # pylint: disable=redefined-outer-name
     @app.callback(
         Output('sel-atoms-output', 'children'),
         [Input('mol2d', 'selectedAtomIds')]
