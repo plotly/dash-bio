6115f18026eaa932296d06b31d20908586c9c4c5
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-05-27 12:33:28 -0400

    Add molecule2d component.

---
+++ src/lib/components/Molecule2dViewer.react.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Molecule2d from 'molecule-2d-for-react';
+
+/**
+ *
+ */
+export default class Molecule2dViewer extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeSelection = this.onChangeSelection.bind(this);
+    }
+
+    onChangeSelection(selectedAtomIds) {
+        this.props.setProps({selectedAtomIds: selectedAtomIds});
+    }
+
+    render() {
+        const {id} = this.props;
+
+        return (
+            <div id={id}>
+                <Molecule2d
+                    {...this.props}
+                    onChangeSelection={this.onChangeSelection}
+                />
+            </div>
+        );
+    }
+}
+
+Molecule2dViewer.propTypes = {
+    /**
+     * The ID used to identify this component in callbacks
+     */
+    id: PropTypes.string,
+
+    /**
+     * Dash-assigned callback that should be called whenever properties change
+     */
+    setProps: PropTypes.func,
+
+    /**
+     * The selected atom IDs.
+     */
+    selectedAtomIds: PropTypes.arrayOf(PropTypes.number),
+
+    /**
+     * The width of the SVG element.
+     */
+    width: PropTypes.number,
+
+    /**
+     * The height of the SVG element.
+     */
+    height: PropTypes.number,
+
+    /**
+     * Description of the molecule to display.
+     */
+    modelData: PropTypes.shape({
+        nodes: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.number,
+                atom: PropTypes.string,
+            })
+        ),
+        links: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.number,
+                source: PropTypes.number,
+                target: PropTypes.number,
+                bond: PropTypes.number,
+                strength: PropTypes.number,
+                distance: PropTypes.number,
+            })
+        ),
+    }),
+};
