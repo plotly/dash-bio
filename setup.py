a80a7b9a87876d2beeeb57505f1411d881ac9cde
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-05-29 12:04:53 -0400

    Fix alphabetical ordering of setup install requirements.

--- setup.py
+++ setup.py
@@ -29,9 +29,9 @@ setup(
         'biopython',
         'colour',
         'GEOparse>=1.1.0',
-        'periodictable',
         'pandas',
-        'parmed'
+        'parmed',
+        'periodictable'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
