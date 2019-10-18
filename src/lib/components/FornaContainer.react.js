9411e5da5f8d3fd11e2527e118ae612d015dd42d
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-10-18 11:08:09 -0400

    Add back renderNewSequences to componentDidMount.

--- src/lib/components/FornaContainer.react.js
+++ src/lib/components/FornaContainer.react.js
@@ -18,6 +18,7 @@ export default class FornaContainer extends Component {
         this._fornaContainer = new PreFornaContainer('#' + id, {
             initialSize: [width, height],
         });
+	this.renderNewSequences();
     }
 
     componentDidUpdate() {
