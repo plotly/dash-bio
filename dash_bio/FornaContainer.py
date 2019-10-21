cf6bd8fd6997cb03d912cf16cb5d05ac99fb220a
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-10-21 16:23:20 -0500

    Update package version and all autogenerated files.

--- dash_bio/FornaContainer.py
+++ dash_bio/FornaContainer.py
@@ -17,18 +17,64 @@ components in an app.
 be displayed.
 - width (number; default 300): The width (in px) of the container in which the molecules will
 be displayed.
-- sequences (list; optional): The molecules that will be displayed.
+- sequences (dict; optional): The molecules that will be displayed. sequences has the following type: list of dicts containing keys 'sequence', 'structure', 'options'.
+Those keys have the following types:
+  - sequence (string; required): A string representing the RNA nucleotide sequence of
+the RNA molecule.
+  - structure (string; required): A dot-bracket string
+(https://software.broadinstitute.org/software/igv/RNAsecStructure)
+that specifies the secondary structure of the RNA
+molecule.
+  - options (dict; optional): Additional options to be applied to the rendering of
+the RNA molecule. options has the following type: dict containing keys 'applyForce', 'circularizeExternal', 'labelInterval', 'name', 'avoidOthers'.
+Those keys have the following types:
+  - applyForce (boolean; optional): Indicate whether the force-directed layout will be
+applied to the displayed molecule. Enabling this
+option allows users to change the layout of the
+molecule by selecting and dragging the individual
+nucleotide nodes. True by default.
+  - circularizeExternal (boolean; optional): This only makes sense in connection with the
+applyForce argument. If it's true, the external
+loops will be arranged in a nice circle. If false,
+they will be allowed to flop around as the force
+layout dictates. True by default.
+  - labelInterval (number; optional): Change how often nucleotide numbers are labelled
+with their number. 10 by default.
+  - name (string; optional): The molecule name; this is used in custom color
+scales.
+  - avoidOthers (boolean; optional): Whether or not this molecule should "avoid" other
+molecules in the map.
 - nodeFillColor (string; optional): The fill color for all of the nodes. This will override any
 color scheme defined in colorScheme.
 - colorScheme (a value equal to: 'sequence', 'structure', 'positions', 'custom'; default 'sequence'): The color scheme that is used to color the nodes.
-- customColors (optional): The custom colors used to color the nodes if the 'custom'
+- customColors (dict; optional): The custom colors used to color the nodes if the 'custom'
 option is chosen for the `colorScheme` prop.
 For example, if the domain is `[0, 20]`, the range is
 `['yellow', 'red']`, and the dictionary specified in
 'colorValues' that corresponds to a molecule is `{'6': 10}`,
 the sixth nucleotide in that molecule will have a color that is
 perfectly in between yellow and red (i.e., orange), since 10 is
-perfectly in between 0 and 20.
+perfectly in between 0 and 20. customColors has the following type: dict containing keys 'domain', 'range', 'colorValues'.
+Those keys have the following types:
+  - domain (list of numbers; optional): The limits for the color scale. This is used with the range
+specified in `range` to calculate the color of a given
+nucleotide, based on the number that it is assigned.
+  - range (list of strings; optional): The range of colors that will be used in conjunction with
+the `domain` prop.
+  - colorValues (dict with strings as keys and values of type dict with strings as keys and values of type string | number; optional): A dictionary which contains keys, each of which are either
+an empty string (`''`) or the name of a molecule that has
+been defined in the `name` prop in the `options` for a
+sequence in the `sequences` property.
+The value corresponding to the key that is an empty string
+(if that key exists) is a "default" color scheme that will
+be applied first, and can be overridden by the color
+schemes defined for molecule-specific keys. The
+aforementioned color schemes each take the form of a
+dictionary in which the keys are the nucleotide positions
+and the values are either a) numbers to be normalized with
+respect to the scale defined in `domain` (so that their
+color will be calculated), or b) direct string
+representations of colors.
 - allowPanningAndZooming (boolean; default True): Allow users to zoom in and pan the display. If this is enabled,
 then pressing the 'c' key on the keyboard will center the view."""
     @_explicitize_args
