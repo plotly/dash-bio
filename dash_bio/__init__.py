e4c366802b6cade1b917240f01212d5c3e43732e
cytham <e0054302@u.nus.edu>
2020-06-09 20:59:23 +0800

    Add variantmap

--- dash_bio/__init__.py
+++ dash_bio/__init__.py
@@ -9,6 +9,7 @@ import dash as _dash
 from .component_factory._manhattan import ManhattanPlot
 from .component_factory._volcano import VolcanoPlot
 from .component_factory._clustergram import Clustergram
+from .component_factory._variant import VariantMap
 
 if not hasattr(_dash, 'development'):
     print('Dash was not successfully imported. '
