09faa474ffa6228e612cdc46c9a98d1ceb31c263
xhlulu <xhlperso@gmail.com>
2021-02-10 22:05:04 -0500

    Remove filepath and package from setup.py

--- setup.py
+++ setup.py
@@ -2,27 +2,17 @@ import os
 import io
 from setuptools import setup
 
-filepath = os.path.abspath(os.path.dirname(__file__))
-
 long_description = io.open('README.md', encoding='utf-8').read()
 
-package = {
-    'name': 'dash_bio_utils',
-    'version': '0.0.5',
-    'author': 'The Plotly Team',
-    'author_email': 'dashbio@plot.ly',
-    'description': 'Simple parsing tools that supplement dash-bio.'
-}
-
 setup(
-    name=package['name'],
-    version=package['version'],
+    name='dash_bio_utils',
+    version='0.0.5',
     url='http://github.com/plotly/dash-bio-utils',
-    author=package['author'],
+    author='The Plotly Team',
     author_email='dashbio@plot.ly',
-    packages=[package['name']],
+    packages=['dash_bio_utils'],
     include_package_data=True,
-    description=package['description'],
+    description='Simple parsing tools that supplement dash-bio.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
