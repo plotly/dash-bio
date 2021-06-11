aa7bbfe90ad3277bf3a86f52cb3e3f0dd9c763f2
Hammad Khan <hammadtheone@gmail.com>
2021-06-11 16:44:13 -0400

    Added glviewer instance for zoom update

--- src/lib/fragments/Molecule3dViewer.js
+++ src/lib/fragments/Molecule3dViewer.js
@@ -14,11 +14,14 @@ export default class Molecule3dViewer extends Component {
     }
 
     onRenderNewData(glviewer) {
+        this.glviewer = glviewer;
+
         glviewer.zoomTo(
             this.props.zoomTo.sel,
             this.props.zoomTo.animationDuration,
             this.props.zoomTo.fixedPath
         );
+
         glviewer.zoom(
             this.props.zoom.factor,
             this.props.zoom.animationDuration,
@@ -35,6 +38,7 @@ export default class Molecule3dViewer extends Component {
             this.props.selectionType !== nextProps.selectionType ||
             this.props.orbital !== nextProps.orbital ||
             this.props.shapes !== nextProps.shapes ||
+            this.props.labels !== nextProps.labels ||
             this.props.zoom !== nextProps.zoom ||
             this.props.zoomTo !== nextProps.zoomTo ||
             (!this.props.selectedAtomIds && nextProps.selectedAtomIds) ||
@@ -55,6 +59,26 @@ export default class Molecule3dViewer extends Component {
             return true;
         }
         return false;
+    }
+
+    componentDidUpdate(prevProps) {
+        if (
+            (this.props.zoom !== prevProps.zoom ||
+                this.props.zoomTo !== prevProps.zoomTo) &&
+            this.glviewer
+        ) {
+            this.glviewer.zoomTo(
+                this.props.zoomTo.sel,
+                this.props.zoomTo.animationDuration,
+                this.props.zoomTo.fixedPath
+            );
+
+            this.glviewer.zoom(
+                this.props.zoom.factor,
+                this.props.zoom.animationDuration,
+                this.props.zoom.fixedPath
+            );
+        }
     }
 
     render() {
