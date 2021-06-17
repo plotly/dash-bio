5652bf01ea05c0b5e30472cdd91de8b52fef2932
Hammad Khan <hammadtheone@gmail.com>
2021-06-17 00:01:16 -0400

    Update import with build process

--- dash_bio/__init__.py
+++ dash_bio/__init__.py
@@ -24,13 +24,8 @@ with open(_filepath) as f:
 package_name = package['name'].replace(' ', '_').replace('-', '_')
 __version__ = package['version']
 
-_current_path = _os.path.dirname(_os.path.abspath(__file__))
-_components = _dash.development.component_loader.load_components(
-    _os.path.join(_current_path, 'metadata.json'),
-    package_name
-)
-
-_this_module = _sys.modules[__name__]
+from ._imports_ import *  # noqa: F401, F403, E402
+from ._imports_ import __all__  # noqa: E402
 
 async_resources = [
     'alignment',
@@ -115,7 +110,6 @@ _js_dist.extend(
 _css_dist = []
 
 
-for _component in _components:
-    setattr(_this_module, _component.__name__, _component)
-    setattr(_component, '_js_dist', _js_dist)
-    setattr(_component, '_css_dist', _css_dist)
+for _component in __all__:
+    setattr(locals()[_component], "_js_dist", _js_dist)
+    setattr(locals()[_component], "_css_dist", _css_dist)
