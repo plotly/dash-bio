c17d3ff032c06f134a70c374e0e7770f16bd5c23
nickmelnikov82 <nickmelnikov82@gmail.com>
2021-08-28 20:21:23 +0300

    Add null handling for Circos tooltip content

--- src/lib/fragments/Circos.react.js
+++ src/lib/fragments/Circos.react.js
@@ -109,7 +109,10 @@ export default class Circos extends Component {
          * Set the tool tip event handler. It allows the user to specify what data they want
          * to show on annotation click or hover
          */
-        if (typeof configApply.tooltipContent !== 'undefined') {
+        if (
+            typeof configApply.tooltipContent !== 'undefined' &&
+            configApply.tooltipContent !== null
+        ) {
             if (typeof configApply.tooltipContent.name !== 'undefined') {
                 if (configApply.tooltipContent.name === 'all') {
                     configApply.tooltipContent = d => {
