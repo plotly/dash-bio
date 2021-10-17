5c8cf01c4b7a717822c16347e37dd42cba4e9aea
Hammad Khan <hammadtheone@gmail.com>
2021-10-17 17:39:53 -0400

    Updated test

--- tests/integration/test_variant_map.py
+++ tests/integration/test_variant_map.py
@@ -5,25 +5,26 @@ import dash_bio
 import dash_core_components as dcc
 
 
-# Load dataframe and metadata
-_data_file = 'tests/dashbio_demos/dash-variant-map/data/sample_data.h5'
-with pd.HDFStore(_data_file, mode="r") as store:
-    _data = store['dataset']
-    metadata = store.get_storer('dataset').attrs.metadata
-
-# Add metadata to dataframe
-_data.metadata = ''
-_data.metadata = metadata
-
-
 def test_dbvm001_basic(dash_duo):
 
     app = dash.Dash(__name__)
+
+    # Load dataframe and metadata
+    _data_file = 'tests/integration/assets/variantmap/sample_data.h5'
+
+    with pd.HDFStore(_data_file, mode="r") as store:
+        _data = store['dataset']
+        metadata = store.get_storer('dataset').attrs.metadata
+
+    # Add metadata to dataframe
+    _data.metadata = ''
+    _data.metadata = metadata
+
     app.layout = dcc.Graph(
         id="variantmap",
         figure=dash_bio.VariantMap(_data)
     )
 
     dash_duo.start_server(app)
-    dash_duo.wait_for_element('#variantmap')
+    dash_duo.wait_for_element('.js-plotly-plot')
     dash_duo.percy_snapshot('variantmap-basic')
