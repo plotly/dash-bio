f37a7f02b8d5848947e1e2af4a06fc42d6ab4cd7
nickmelnikov82 <nickmelnikov82@gmail.com>
2021-10-18 17:55:19 +0300

    Tests for new properties.

--- tests/integration/test_alignment_chart.py
+++ tests/integration/test_alignment_chart.py
@@ -92,3 +92,56 @@ def test_dbav003_change_conservation_colorscale(dash_duo):
         bars[1].get_attribute('style')
     )
     assert match.group(1) == 'rgb(230, 103, 0)'
+
+def test_dbav004_width(dash_duo):
+
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div(simple_app_layout(
+        dash_bio.AlignmentChart(id=_COMPONENT_ID, data=_data)
+    ))
+
+    new_width = 1200
+
+    simple_app_callback(
+        app,
+        dash_duo,
+        component_id=_COMPONENT_ID,
+        test_prop_name='width',
+        test_prop_value=new_width,
+        prop_value_type='int',
+        validation_fn=lambda x: x == new_width
+    )
+
+    main = dash_duo.find_element(f'#{_COMPONENT_ID}')
+
+    assert int(main.size['width']) == new_width
+
+
+def test_dbnp005_height(dash_duo):
+    """ Test that check if width property is setting correctly """
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div(simple_app_layout(
+        dash_bio.NeedlePlot(
+            id=_COMPONENT_ID,
+            mutationData=_data
+        )
+    ))
+
+    new_height = 350
+
+    simple_app_callback(
+        app,
+        dash_duo,
+        component_id=_COMPONENT_ID,
+        test_prop_name='height',
+        test_prop_value=new_height,
+        validation_fn=lambda x: x == new_height,
+        prop_value_type='int',
+        take_snapshot=True
+    )
+
+    main = dash_duo.find_element(f'#{_COMPONENT_ID}')
+
+    assert int(main.size['height']) == new_height
