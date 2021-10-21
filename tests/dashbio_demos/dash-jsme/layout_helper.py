7077499c36227350f02159fba004a0750846106c
nickmelnikov82 <nickmelnikov82@gmail.com>
2021-10-21 13:11:43 +0300

    Fixed dash python imports.

--- tests/dashbio_demos/dash-jsme/layout_helper.py
+++ tests/dashbio_demos/dash-jsme/layout_helper.py
@@ -2,8 +2,8 @@ import base64
 import os
 
 import dash
-from dash import dcc
-from dash import html
+import dash_core_components as dcc
+import dash_html_components as html
 
 
 def run_standalone_app(
