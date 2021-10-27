ec75d127afe2cad0be587b434686a64c1f7a0126
olayway <52197250+olayway@users.noreply.github.com>
2021-10-27 03:49:55 +0200

    fix: pdb parser refactor (#18)
    * refactored pdb_parser as a class to enable mol3d, mol2d and spec compatible data creation; utilizes ParmEd library
    
    * methods docstrings added
    
    * misspels fixes
    
    * refactored molecule3dviewer styles creator
    
    * renamed styles creator function
    
    * style creator constants added
    
    * removed mmcif parser
    
    * removed comment
    
    * imported PdbParser and create_mol3d_style in dash_bio_utils init file
    
    Co-authored-by: Aleksandra Rubaj <aleksandra.rubaj@raffine.eu>

---
+++ dash_bio_utils/__init__.py
@@ -0,0 +1,2 @@
+from .pdb_parser import PdbParser
+from .mol3dviewer_styles_creator import create_mol3d_style
--- dash_bio_utils/pdb_parser.py
+++ dash_bio_utils/pdb_parser.py
@@ -1,147 +1,98 @@
 """PDB parser
 
-This module contains functions that can read PDB files and return a
-JSON representation of the structural and styling data."""
+This module contains a class that can read PDB files and return a dictionary of structural data"""
 
 import re
-import six.moves.urllib.request as urlreq
-from six.moves.urllib.parse import urlparse as urlparse
-from six.moves.urllib.error import URLError
+
 import parmed as pmd
 
-from .styles import (
-    ATOM_COLOR_DICT,
-    CHAIN_COLOR_DICT,
-    RESIDUE_COLOR_DICT,
-    RESIDUE_TYPES,
-    RESIDUE_TYPE_COLOR_DICT,
-    create_style
-)
 
-
-def create_data(pdb_path, style="cartoon", mol_color="residue", **kwargs):
+class PdbParser:
     """
     Parse the protein data bank (PDB) file to generate
-    input modelData for the Molecule3dViewer component
-    with the specified style parameters. This function outputs
-    the molData and styles as a dict of JSON formatted values.
+    input data for Molecule3dViewer, Molecule2dViewer and Speck components.
 
     @param pdb_path
-    Name of the biomolecular structure file in PDB format
-    @param style
-    Type of representation of the biomolecule (options: stick, cartoon, sphere)
-    @param mol_color
-    Coloring scheme for depicting biomolecules (options: residue_type, atom, residue, chain)
-    @param custom_dict
-    optional parameter to specify the color scheme for different chains
-    in JSON format
-    @param atm_color
-    optional parameter to specify the color scheme for different atoms
-    in JSON format
+    Path to PDB file (either HTTP or local path).
     """
 
-    output_dict = {}
+    def __init__(self, s: str):
 
-    # Use parmed to read the bond information pdb file
-    top = pmd.load_file(pdb_path)
+        if re.search(r'^[1-9][0-9A-z]{3}$', s):
+            # d is PDB id (https://proteopedia.org/wiki/index.php/PDB_code)
+            self.structure = pmd.download_PDB(s)
+        else:
+            # d is HTTP url or local file
+            self.structure = pmd.load_file(s)
 
-    # Check if the pdb_path points to a URL and decode it if necessary
-    if urlparse(pdb_path).scheme != "":
-        try:
-            file = urlreq.urlopen(pdb_path)
-            lines = [
-                l.decode("utf-8").strip() for l in file if l.decode("UTF-8").strip()
-            ]
-        except URLError as e:
-            print(e.reason)
+        self.atoms = self.structure.atoms
+        self.bonds = self.structure.bonds
 
-    else:
-        # Read PDB file to create atom/bond information
-        with open(pdb_path, "r") as infile:
-            # store only non-empty lines
-            lines = [l.strip() for l in infile if l.strip()]
+    def mol3d_data(self) -> dict:
+        """
+        Generate input data for Molecule3dViewer component.
+        """
 
-    # Initialize all variables
-    var_nchains = []
-    serial = []
-    atm_name = []
-    res_name = []
-    chain = []
-    res_id = []
-    positions = []
-    occupancy = []
-    temp_factor = []
-    atom_type = []
-    ct = 0
+        data = {'atoms': [], 'bonds': []}
 
-    modelData = {"atoms": [], "bonds": []}
+        for a in self.atoms:
+            data["atoms"].append({
+                "serial": a.idx,
+                "name": a.name,
+                "elem": a.element_name,
+                "positions": [a.xx, a.xy, a.xz],
+                "mass_magnitude": a.mass,
+                "residue_index": a.residue.idx,
+                "residue_name": a.residue.name,
+                "chain": a.residue.chain,
 
-    # Variables that store the character positions of different
-    # parameters from the molecule PDB file
-    serialpos = [6, 11]
-    atm_namepos = [12, 16]
-    r_namepos = [17, 20]
-    chainpos = [21, 22]
-    r_idpos = [22, 26]
-    xpos = [30, 38]
-    ypos = [38, 46]
-    zpos = [46, 54]
-    occupos = [54, 60]
-    bfacpos = [60, 66]
-    atm_typepos = [77, 79]
+            })
 
-    for l in lines:
-        line = l.split()
-        if "ATOM" in line[0] or "HETATM" in line[0]:
-            serial.append(int(l[serialpos[0] : serialpos[1]]))
-            atm_name.append(l[atm_namepos[0] : atm_namepos[1]].strip())
-            val_r_name = l[r_namepos[0] : r_namepos[1]].strip()
-            res_name.append(val_r_name)
-            chain_val = l[chainpos[0] : chainpos[1]].strip()
-            chain.append(chain_val)
-            if chain_val not in var_nchains:
-                var_nchains.append(chain_val)
-            val_r_id = int(l[r_idpos[0] : r_idpos[1]])
-            res_id.append(val_r_id)
-            x = float(l[xpos[0] : xpos[1]])
-            y = float(l[ypos[0] : ypos[1]])
-            z = float(l[zpos[0] : zpos[1]])
-            positions.append([x, y, z])
-            occupancy.append(l[occupos[0] : occupos[1]].strip())
-            temp_factor.append(l[bfacpos[0] : bfacpos[1]].strip())
-            atom_type.append(l[atm_typepos[0] : atm_typepos[1]].strip())
-            ct += 1
+        for b in self.bonds:
+            data["bonds"].append({
+                "atom1_index": b.atom1.idx,
+                "atom2_index": b.atom2.idx,
+                "bond_order": b.order
+            })
 
-    # Create list of atoms
-    tmp_res = res_id[0]
-    resct = 1
-    for i in range(len(chain)):  # pylint: disable=consider-using-enumerate
-        if tmp_res != res_id[i]:
-            tmp_res = res_id[i]
-            resct += 1
-        modelData["atoms"].append(
-            {
-                "name": atm_name[i],
-                "chain": chain[i],
-                "positions": positions[i],
-                "residue_index": resct,
-                "element": atom_type[i],
-                "residue_name": res_name[i] + str(res_id[i]),
-                "serial": i,
-            }
-        )
+        return data
 
-    # Create list of bonds using the parmed module
-    for i in range(len(top.bonds)):
-        bondpair = top.bonds[i].__dict__
-        atom1 = re.findall(r"\[(\d+)\]", str(bondpair["atom1"]))
-        atom2 = re.findall(r"\[(\d+)\]", str(bondpair["atom2"]))
-        modelData["bonds"].append(
-            {"atom2_index": int(atom1[0]), "atom1_index": int(atom2[0])}
-        )
+    def mol2d_data(self) -> dict:
+        """
+        Generate input data for Molecule2dViewer component.
+        """
 
-    styles_data = create_style(path=pdb_path, lines=lines, style=style, mol_color=mol_color, **kwargs)
+        data = {'nodes': [], 'links': []}
 
-    output_dict['mol3d'] = {"styles": styles_data, "modelData": modelData}
+        for a in self.atoms:
+            data["nodes"].append({
+                "id": a.idx,
+                "atom": a.element_name
+            })
 
-    return output_dict
+        for i, b in enumerate(self.bonds):
+            data["links"].append({
+                "id": i,
+                "source": b.atom1.idx,
+                "target": b.atom2.idx,
+                "bond": b.order,
+                "distance": b.measure(),
+                "strength": b.energy() # TODO verify
+            })
+
+        return data
+
+    def speck_data(self) -> list:
+        """
+        Generate input data for Speck component.
+        """
+        data = []
+        for a in self.atoms:
+            data.append({
+                "symbol": a.element_name,
+                "x": a.xx,
+                "y": a.xy,
+                "z": a.xz
+            })
+
+        return data
