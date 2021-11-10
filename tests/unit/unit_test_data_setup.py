28a237373a86b0a3917a27c3331f67420375c8db
nickmelnikov82 <nickmelnikov82@gmail.com>
2021-11-10 11:19:33 +0200

    Moved utils tests to unit folder

--- tests/unit/unit_test_data_setup.py
+++ tests/unit/unit_test_data_setup.py
@@ -3,23 +3,23 @@ import json
 from dash_bio.utils import mol3dviewer_styles_creator, pdb_parser,\
     protein_reader, xyz_reader
 
-pdb = pdb_parser.PdbParser('tests/dashbio_demos/dash-molecule-3d-viewer/data/1bna.pdb')
-with open('tests/unit/mol3d_model_data.json', 'w+') as f:
+pdb = pdb_parser.PdbParser('../dashbio_demos/dash-molecule-3d-viewer/data/1bna.pdb')
+with open('mol3d_model_data.json', 'w+') as f:
     f.write(json.dumps(pdb.mol3d_data()))
 
-with open('tests/unit/mol3d_styles_data.json', 'w+') as f:
+with open('mol3d_styles_data.json', 'w+') as f:
     f.write(json.dumps(mol3dviewer_styles_creator.create_mol3d_style(
         pdb.mol3d_data().get("atoms"),
         'cartoon',
         'atom'
     )))
 
-with open('tests/unit/sequence_viewer_data.json', 'w+') as f:
+with open('sequence_viewer_data.json', 'w+') as f:
     f.write(json.dumps(protein_reader.read_fasta(
-        datapath_or_datastring='tests/dashbio_demos/dash-sequence-viewer/data/P01308.fasta.txt'
+        datapath_or_datastring='../dashbio_demos/dash-sequence-viewer/data/P01308.fasta.txt'
     )[0]))
 
-with open('tests/unit/speck_data.json', 'w+') as f:
+with open('speck_data.json', 'w+') as f:
     f.write(json.dumps(xyz_reader.read_xyz(
-        datapath_or_datastring='tests/dashbio_demos/dash-speck/data/methane.xyz'
+        datapath_or_datastring='../dashbio_demos/dash-speck/data/methane.xyz'
     )))
