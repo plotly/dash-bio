569399c45857ef3de9066b3a080b4a0d6749b46d
Hammad Khan <hammadtheone@gmail.com>
2021-11-19 15:03:04 -0500

    Linting fixes

--- dash_bio/component_factory/_clustergram.py
+++ dash_bio/component_factory/_clustergram.py
@@ -184,7 +184,6 @@ Keyword arguments:
     kwargs.pop("computed_traces")
     kwargs.pop("generate_curves_dict")
 
-
     (fig, ct, curves_dict) = _Clustergram(**kwargs).figure(
         computed_traces=computed_traces
     )
