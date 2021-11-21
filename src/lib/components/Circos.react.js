cad2f33ce8059de8c0a0820d4ed669a1e13d69c8
Aleksandra Rubaj <aleksandra.rubaj@raffine.eu>
2021-11-21 01:27:45 +0100

    fixes to Dash Bio Circos property types and descripitons, build

--- src/lib/components/Circos.react.js
+++ src/lib/components/Circos.react.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Component, lazy, Suspense } from 'react';
+import React, {Component, lazy, Suspense} from 'react';
 import LazyLoader from '../LazyLoader';
 
 const RealCircos = lazy(LazyLoader.circos);
